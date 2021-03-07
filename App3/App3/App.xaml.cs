856b4d0a76efcb52b8601a79aacfdab494d4ff21
Prince Pluxury <grisha.kluchka404@gmail.com>
2021-03-07 16:47:46 +0300

    Демонстрация жизненного цикла

---
+++ App3/App3/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            MainPage.DisplayAlert("Message", "OnStart", "Continue");
+        }
+
+        protected override void OnSleep()
+        {
+            MainPage.DisplayAlert("Message", "OnSleep", "Continue");
+        }
+
+        protected override void OnResume()
+        {
+            MainPage.DisplayAlert("Message", "OnResume", "Continue");
+        }
+    }
+}
